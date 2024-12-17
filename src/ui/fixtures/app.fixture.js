0e437336d76632e13e6bb3a4e7ac764ec089f870
olemoreflipflop <fil.olga.ev@gmail.com>
2024-12-17 17:11:47 +0300

    test

--- src/ui/fixtures/app.fixture.js
+++ src/ui/fixtures/app.fixture.js
@@ -2,9 +2,9 @@ import { test as base } from '@playwright/test';
 import { App } from '../pages';
 
 export const test = base.extend({
-  baseApp: async ({ page }, use) => {
+  baseApp: async ({ page, baseURL }, use) => {
     const baseApp = new App(page);
-    await baseApp.page.goto(process.env.BASE_URL);
+    await baseApp.page.goto(`https://ohmywishes.com`);
     await use(baseApp);
     await baseApp.page.close();
   },
